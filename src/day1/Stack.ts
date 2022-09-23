4226133e0726f6cf9639b0bd3e4b659753d40911
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-09-23 18:23:40 +0200

    implement stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,80 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
+        const node = { value: item } as Node<T>;
 
-}
+        this.length++;
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+
+        node.prev = this.head;
+        this.head = node;
+    }
+
     pop(): T | undefined {
+        this.length = Math.max(0, this.length - 1);
+        if (this.length === 0) {
+            const head = this.head;
+            this.head = undefined;
+            return head?.value;
+        }
 
-}
+        const head = this.head as Node<T>;
+        this.head = head.prev;
+        return head.value;
+    }
+
     peek(): T | undefined {
-
+        return this.head?.value;
+    }
 }
+
+type StackNode<T> = {
+    item: T;
+    next?: StackNode<T>;
+};
+class MyStack<T> {
+    public length: number;
+    private head?: StackNode<T>;
+
+    constructor() {
+        this.length = 0;
+    }
+
+    push(item: T): void {
+        const newNode = { item } as StackNode<T>;
+        newNode.next = this.head;
+        this.head = newNode;
+
+        this.length++;
+    }
+
+    pop(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
+
+        const head = this.head;
+        this.head = this.head?.next;
+        this.length--;
+        return head?.item;
+    }
+
+    peek(): T | undefined {
+        return this.head?.item;
+    }
 }
