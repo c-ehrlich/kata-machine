00e377e218baa79930c43598a77d9543a634d8b8
c-ehrlich <ehrlich.christopher@gmail.com>
2023-12-09 22:46:59 +0100

    binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,37 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
 
+    do {
+        const m = Math.floor(lo + (hi - lo) / 2);
+        const v = haystack[m];
+        if (v === needle) return true;
+        if (v > needle) hi = m;
+        if (v < needle) lo = m + 1;
+    } while (lo < hi);
+
+    return false;
 }
+
+// my version
+
+// export default function bs_list(haystack: number[], needle: number): boolean {
+//   return bsearch(haystack, needle, 0, haystack.length - 1) ?? false;
+// }
+
+// function bsearch(
+//   haystack: number[],
+//   needle: number,
+//   lo: number,
+//   hi: number,
+// ): boolean {
+//   const mid = Math.floor(lo + (hi - lo) / 2);
+//   const val = haystack[mid];
+//   if (val === needle) return true;
+//   if (hi <= lo) return false;
+
+//   if (needle > val) return bsearch(haystack, needle, mid + 1, hi);
+//   if (needle < val) return bsearch(haystack, needle, lo, mid - 1);
+
+//   return false;
+// }
