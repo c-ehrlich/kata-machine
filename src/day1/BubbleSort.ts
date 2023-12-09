8ef34fab26b04b4f9fe9edc144ed89a142792536
c-ehrlich <ehrlich.christopher@gmail.com>
2023-12-09 22:47:57 +0100

    bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,24 @@
 export default function bubble_sort(arr: number[]): void {
-
+    for (let i = 0; i < arr.length; ++i) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                const tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
 }
+
+// my version
+// export default function bubble_sort(arr: number[]): void {
+//     for (let max = arr.length - 1; max >= 1; max--) {
+//         for (let i = 1; i <= max; i++) {
+//             if (arr[i - 1] > arr[i]) {
+//                 const temp = arr[i - 1];
+//                 arr[i - 1] = arr[i];
+//                 arr[i] = temp;
+//             }
+//         }
+//     }
+// }
